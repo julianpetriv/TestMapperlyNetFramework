b93db7f5cbf806b69c00db729874d49b368383f0
julianpetriv <petriv.j@gmail.com>
2024-01-25 12:07:48 +0200

    Add project files.

---
+++ TestMapperlyNetFramework/TestMapper.cs
@@ -0,0 +1,15 @@
+﻿using Riok.Mapperly.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMapperlyNetFramework
+{
+    [Mapper]
+    public partial class TestMapper
+    {
+        public partial Dog MapAnimalToDog(Animal animal);
+    }
+}
